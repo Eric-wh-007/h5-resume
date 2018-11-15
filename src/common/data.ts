9542e9fe588837ccd7037581d8f7665b48371544
wanghang <wanghang@gagogroup.com>
2018-11-15 10:34:23 +0800

    complete

--- src/common/data.ts
+++ src/common/data.ts
@@ -16,17 +16,17 @@ const head = {
   links: [
     {
       icon: githubSrc,
-      href: '/',
+      href: 'https://github.com/Eric-wh-007',
       name: 'github'
     },
     {
       icon: blogSrc,
-      href: '/',
+      href: 'https://www.jianshu.com/u/a1838b5b5d28',
       name: 'blog'
     },
     {
       icon: websiteSrc,
-      href: '/',
+      href: 'http://www.wanghang.cool',
       name: 'website'
     },
   ]
@@ -60,16 +60,18 @@ const workExp = [
     position: '前端实习生',
     time: '2017.10 - 2018.1',
     icon: tencentSrc,
-    desc: '在职期间负责 “互联网+ 项目管理系统” 的开发与维护，在开发新功能的同时改造老旧代码，重构了部分代码，使得开发更高效，bug率明显减少，并独立开发了微信端，解决了pc的限制，随时随地都可办公。',
+    desc: '在职期间负责 “互联网+ 项目管理系统” 的开发与维护，\
+    在开发新功能的同时改造老旧代码，重构了部分代码，使得开发更高效，bug率明显减少，\
+    并独立开发了微信端页面，解决了pc的限制，随时随地都可办公。',
   },
   {
     company: '佳格天地',
     position: '前端开发',
     time: '2018.3 - 至今',
     icon: gagoSrc,
-    desc: '在职期间负责老项目 GagoEngine GagoMill 的开发与维护 以及新项目 GagoPublisher的开发\
-      其中GagoEngine 为Angular开发 为此用了一周时间快速学习Angular达到了可以上手的程度。\
-      GagoPublisher前后台均为个人独立开发，本着“高内聚、低耦合”的原则，代码实现非常优雅。',
+    desc: '在职期间负责项目 GagoEngine、 demo管理系统 的开发与维护 以及新项目 GagoPublisher的开发\
+      在职期间通过学习校内无法学到的但是实际项目需要的用到的软硬技能，快速成长为一名合格的前端工程师。\
+      自己的能力也得到同事及上司的认可。',
   },
 ];
 const projectExp = [
